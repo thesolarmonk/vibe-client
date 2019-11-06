a4bcd05528c896264c1de9f936ab828df8d8cc49
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-06 01:26:48 -0800

    [VUEX]: Add global variables to track user authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+// import { stat } from 'fs';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    authenticated: false,
+    access_token: ''
+  },
+  getters: {
+    isAuthenticated: state => {
+      return state.authenticated;
+    },
+    getAccessToken: state => {
+      return state.access_token;
+    }
   },
   mutations: {
+    login(state, access_token) {
+      state.authenticated = true;
+      state.access_token = access_token;
+    },
+    logout(state) {
+      state.authenticated = false;
+      state.access_token = '';
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
