2f56aa2b671b8da0844ea910e449c1605b0f21b8
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-06 00:10:56 -0800

    [ROUTER]: Setup app routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../views/Login.vue';
+import Feed from '../views/Feed.vue';
+import Profile from '../views/Profile.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/feed',
+    name: 'feed',
+    component: Feed
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
