7550a3ff456afc4790b2e7fc8111c36d20f3eed9
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-06 00:14:01 -0800

    [COMPONENT]: Add Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button id="login with spotify" @click="login()">Login with Spotify</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      spotifyAuthUrl:
+        "https://accounts.spotify.com/authorize?client_id=5ae1d35a487842fcbef9cb7f164ec3fb&response_type=token&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2Ffeed&scope=streaming%20user-read-email%20user-read-private"
+    };
+  },
+  methods: {
+    login: function() {
+      fetch(this.spotifyAuthUrl, {
+        method: "GET"
+      }).then(response => {
+        window.location.href = response.url;
+        // console.log("test");
+      });
+    }
+  }
+};
+</script>
