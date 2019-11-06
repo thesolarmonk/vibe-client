342d6bb081e63ea3a5afdf082dc3b0f746698c32
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-05 23:29:56 -0800

    TEST

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Login</router-link>|
+      <router-link to="/feed">Feed</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #000;
+
+  width: 100vw;
+  height: 100vh;
+  margin: 0px;
+  padding: 0px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img @click="login()" alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  methods: {
+    login: function() {
+      fetch(
+        "https://accounts.spotify.com/authorize?client_id=5ae1d35a487842fcbef9cb7f164ec3fb&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Ffeed&scope=streaming%20user-read-email%20user-read-private",
+        {
+          method: "GET"
+        }
+      ).then(response => {
+        window.location.href = response.url;
+        // console.log("test");
+      });
+    }
+  }
+};
+</script>
