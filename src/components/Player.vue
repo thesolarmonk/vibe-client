9a7f6c2cb71d3c2e5e08b2fc2c143b85c011a503
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-08 17:33:53 -0800

    [COMPONENT]: Add code to initialize Spotify Web Player on mount

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,7 +1,67 @@
 <template></template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+
+export default {
+  name: "player",
+  mounted() {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      this.$store.state.player = new window.Spotify.Player({
+        name: "Web Playback SDK Quick Start Player",
+        getOAuthToken: cb => {
+          cb(this.$store.state.access_token);
+        },
+        volume: 1
+      });
+
+      // Error handling
+      this.$store.state.player.addListener(
+        "initialization_error",
+        ({ message }) => {
+          console.error(message);
+        }
+      );
+      this.$store.state.player.addListener(
+        "authentication_error",
+        ({ message }) => {
+          console.error(message);
+        }
+      );
+      this.$store.state.player.addListener("account_error", ({ message }) => {
+        console.error(message);
+      });
+      this.$store.state.player.addListener("playback_error", ({ message }) => {
+        console.error(message);
+      });
+
+      // Playback status updates
+      this.$store.state.player.addListener("player_state_changed", state => {
+        console.log(state);
+      });
+
+      // Ready
+      this.$store.state.player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+        this.$store.state.player_id = device_id;
+      });
+
+      // Not Ready
+      this.$store.state.player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+
+      // Connect to the player!
+      this.$store.state.player.connect().then(success => {
+        if (success) {
+          console.log(
+            "The Web Playback SDK successfully connected to Spotify!"
+          );
+        }
+      });
+    };
+  }
+};
 </script>
 
 <style>
