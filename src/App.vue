c070125adda32ff182312bbb8910f5bcc1bc7222
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-08 17:30:50 -0800

    [COMPONENT]: Add logic to initialize player only when authenticated

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <player v-if="isAuthenticated"></player>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
+import Player from "./components/Player.vue";
+
 export default {
   name: "app",
-  components: {}
+  computed: {
+    ...mapGetters(["isAuthenticated"])
+  },
+  components: {
+    Player
+  }
 };
 </script>
 
