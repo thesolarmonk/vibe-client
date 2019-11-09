81884e54cbd1098b49c3f90e8e07929495bd3a26
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-08 17:36:01 -0800

    [COMPONENT]: Add play functionality to Spotify player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -60,6 +60,29 @@ export default {
         }
       });
     };
+  },
+  computed: {
+    ...mapGetters(["getAccessToken", "getPlayerId"])
+  },
+  methods: {
+    play: ({ spotify_uri, id, access_token }) => {
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          uris: [spotify_uri]
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`
+        }
+      })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
