73bd76b20c902aced844c21ad47da5e1eabee6b2
Jiayu Zhao <jyzhao1230@g.ucla.edu>
2019-11-14 20:41:51 -0800

    [TEST]: Profile component tests

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,86 +1,68 @@
 <template>
-  <div>
-    <v-app id="inspire">
-      <v-card v-bind:style="cardObject">
-        <v-navigation-drawer
-          v-model="sideNav"
-          :mini-variant.sync="mini"
-          permanent
-          disable-route-watcher
-          enable-resize-watcher
-          :clipped="clipped"
-          dark
-          app
-        >
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-img
-                src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
-              ></v-img>
-            </v-list-item-avatar>
+    <div>
+        <v-app id="inspire">
+            <v-card v-bind:style="cardObject">
+                <v-navigation-drawer
+                    v-model="sideNav"
+                    :mini-variant.sync="mini"
+                    permanent
+                    disable-route-watcher
+                    enable-resize-watcher
+                    :clipped="clipped"
+                    dark
+                    app
+                >
+                    <v-list-item>
+                        <v-list-item-avatar>
+                            <v-img
+                                src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
+                            ></v-img>
+                        </v-list-item-avatar>
 
-            <v-list-item-title>Vibe User</v-list-item-title>
+                        <v-list-item-title>Vibe User</v-list-item-title>
 
-            <v-btn icon @click.stop="mini = !mini">
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-          </v-list-item>
+                        <v-btn class="openclose" icon @click.stop="flipMini">
+                            <v-icon>mdi-chevron-left</v-icon>
+                        </v-btn>
+                    </v-list-item>
 
-          <v-divider></v-divider>
+                    <v-divider></v-divider>
 
-          <v-list dense>
-            <v-list-item v-for="item in items" :key="item.title" link>
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
+                    <v-list dense>
+                        <v-list-item v-for="item in items" :key="item.title" link>
+                            <v-list-item-icon>
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-item-icon>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <div v-bind:style="userInfoObject">
-          <figure class="image is-128x128" v-bind:style="profileImageObject">
-            <img
-              class="is-rounded"
-              src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
-            />
-          </figure>
-          <p v-bind:style="userNameObject">User Name</p>
-        </div>
-        <div v-bind:style="trackListObject">
-          <li>track1</li>
-          <li>track2</li>
-        </div>
-        <div v-bind:style="graphsObject">
-          <p>Graphs place holder</p>
-        </div>
-      </v-card>
-    </v-app>
-  </div>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+                <div v-bind:style="userInfoObject">
+                    <figure class="image is-128x128" v-bind:style="profileImageObject">
+                        <img
+                            class="is-rounded"
+                            src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
+                        />
+                    </figure>
+                    <p v-bind:style="userNameObject">User Name</p>
+                </div>
+                <div v-bind:style="trackListObject">
+                    <li>track1</li>
+                    <li>track2</li>
+                </div>
+                <div v-bind:style="graphsObject">
+                    <p>Graphs place holder</p>
+                </div>
+            </v-card>
+        </v-app>
+    </div>
 </template>
 
 <script>
 import {
-  VApp,
-  VCard,
-  VNavigationDrawer,
-  VListItem,
-  VListItemAvatar,
-  VImg,
-  VListItemTitle,
-  VListItemContent,
-  VBtn,
-  VIcon,
-  VDivider,
-  VList,
-  VListItemIcon
-  /* other imports ... */
-} from "vuetify/lib";
-export default {
-  name: "",
-  components: {
     VApp,
     VCard,
     VNavigationDrawer,
@@ -88,48 +70,77 @@ export default {
     VListItemAvatar,
     VImg,
     VListItemTitle,
+    VListItemContent,
     VBtn,
     VIcon,
     VDivider,
     VList,
-    VListItemIcon,
-    VListItemContent
-  },
-  data() {
-    return {
-      drawer: true,
-      items: [
-        { title: "Feed", icon: "mdi-home-city" },
-        { title: "Profile", icon: "mdi-account" },
-        { title: "Search", icon: "mdi-account-group-outline" }
-      ],
-      mini: true,
-      cardObject: {
-        height: "100%"
-      },
-      profileImageObject: {
-        width: "50px",
-        float: "left"
-      },
-      userInfoObject: {
-        width: "100%",
-        marginTop: "75px",
-        marginLeft: "120px",
-        height: "100px"
-      },
-      userNameObject: {
-        display: "table-cell",
-        paddingTop: "15px",
-        paddingLeft: "10px"
-      },
-      trackListObject: {
-        float: "left"
-      },
-      graphsObject: {
-        float: "left",
-        paddingLeft: "50px"
-      }
-    };
-  }
+    VListItemIcon
+    /* other imports ... */
+} from 'vuetify/lib';
+export default {
+    name: '',
+    components: {
+        VApp,
+        VCard,
+        VNavigationDrawer,
+        VListItem,
+        VListItemAvatar,
+        VImg,
+        VListItemTitle,
+        VBtn,
+        VIcon,
+        VDivider,
+        VList,
+        VListItemIcon,
+        VListItemContent
+    },
+    props: {
+        callMe: {
+            type: Function
+        }
+    },
+    data() {
+        return {
+            drawer: true,
+            items: [
+                { title: 'Feed', icon: 'mdi-home-city' },
+                { title: 'Profile', icon: 'mdi-account' },
+                { title: 'Search', icon: 'mdi-account-group-outline' }
+            ],
+            mini: true,
+            cardObject: {
+                height: '100%'
+            },
+            profileImageObject: {
+                width: '50px',
+                float: 'left'
+            },
+            userInfoObject: {
+                width: '100%',
+                marginTop: '75px',
+                marginLeft: '120px',
+                height: '100px'
+            },
+            userNameObject: {
+                display: 'table-cell',
+                paddingTop: '15px',
+                paddingLeft: '10px'
+            },
+            trackListObject: {
+                float: 'left'
+            },
+            graphsObject: {
+                float: 'left',
+                paddingLeft: '50px'
+            }
+        };
+    },
+    methods: {
+        flipMini() {
+            this.mini = !this.mini;
+            this.callMe('flip');
+        }
+    }
 };
 </script>
--- src/test/profile.test.js
+++ src/test/profile.test.js
@@ -1,32 +1,38 @@
-//import Vue from 'vue';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import Profile from '../components/Profile.vue';
+import { mount } from '@vue/test-utils';
+import sinon from 'sinon';
 
+Vue.use(Vuetify);
 // Here are some Jasmine 2.0 tests, though you can
 // use any test runner / assertion library combo you prefer
 describe('Profile', () => {
-    // Inspect the raw component options
-    // it('has a created hook', () => {
-    //     expect(typeof Profile.created).toBe('function');
-    // });
-
     // Evaluate the results of functions in
     // the raw component options
     it('sets the correct default data', () => {
         expect(typeof Profile.data).toBe('function');
-        // const defaultData = MyComponent.data()
-        // expect(defaultData.message).toBe('hello!')
+        const defaultData = Profile.data();
+        expect(defaultData.drawer).toBe(true);
+        expect(defaultData.items.length).toBe(3);
+        expect(defaultData.mini).toBe(true);
     });
 
     // Inspect the component instance on mount
-    it('correctly sets the message when created', () => {
-        // const vm = new Vue(MyComponent).$mount()
-        // expect(vm.message).toBe('bye!')
+    it('Nav bar button clicks are recognized', () => {
+        const spy = sinon.spy();
+        const wrapper = mount(Profile, {
+            Vue,
+            propsData: {
+                callMe: spy
+            }
+        });
+        wrapper.find('v-btn').trigger('click');
+        spy.should.have.been.calledWith('flip');
     });
 
     // Mount an instance and inspect the render output
-    it('renders the correct message', () => {
-        // const Constructor = Vue.extend(MyComponent)
-        // const vm = new Constructor().$mount()
-        // expect(vm.$el.textContent).toBe('bye!')
+    it('register correct methods', () => {
+        expect(typeof Profile.methods.flipMini).toBe('function');
     });
 });
