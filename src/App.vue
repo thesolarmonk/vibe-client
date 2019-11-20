73cfcf50ea215a8cc02979c8d8c594cccf4d7aa8
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-20 15:56:02 -0800

    [STYLE]: Add styles for basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-    <player v-if="isAuthenticated"></player>
+  <div id="app" class="app">
+    <router-view class="app--view"></router-view>
+    <player v-if="isAuthenticated" class="app--nav"></player>
   </div>
 </template>
 
@@ -22,4 +22,27 @@ export default {
 </script>
 
 <style>
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0px;
+}
+
+.app {
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: [view-start] 10fr [view-end nav-start] 1fr [nav-end];
+}
+
+.app--view {
+  grid-row: view-start / view-end;
+  overflow-x: scroll;
+}
+
+.app--nav {
+  grid-row: nav-start / nav-end;
+  overflow: hidden;
+}
 </style>
