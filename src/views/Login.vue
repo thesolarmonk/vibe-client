d82c90837ef526bd6fb8ffbaff2602d0e562aba9
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-20 15:54:28 -0800

    [SPOTIFY]: Update Spotify Web API Login permissions

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>Login</h1>
     <button id="login with spotify" @click="login()">Login with Spotify</button>
   </div>
 </template>
@@ -11,7 +12,7 @@ export default {
   data() {
     return {
       spotifyAuthUrl:
-        "https://accounts.spotify.com/authorize?client_id=5ae1d35a487842fcbef9cb7f164ec3fb&response_type=token&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2Ffeed&scope=streaming%20user-read-email%20user-read-private"
+        "https://accounts.spotify.com/authorize?client_id=5ae1d35a487842fcbef9cb7f164ec3fb&response_type=token&redirect_uri=http%3A%2F%2F127.0.0.1%3A8080%2Ffeed&scope=streaming%20%20user-read-email%20user-read-private%20user-read-recently-played%20user-modify-playback-state%20user-read-playback-state%20playlist-modify-private"
     };
   },
   methods: {
@@ -20,7 +21,6 @@ export default {
         method: "GET"
       }).then(response => {
         window.location.href = response.url;
-        // console.log("test");
       });
     }
   }
