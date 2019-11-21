67b66bb30fe8acad8fce0cfd639e05ab25d345ae
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-20 17:05:05 -0800

    [FIX]: Fix comments

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import router from './router';
 import store from './store';
 import './../node_modules/bulma/css/bulma.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import vuetify from '@/plugins/vuetify'; // path to vuetify export
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 Vue.use(vuetify);
 
 new Vue({
-    router,
-    store,
-    vuetify,
-    render: (h) => h(App)
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app');
