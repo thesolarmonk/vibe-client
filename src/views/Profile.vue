6fc5d3af0fc2a9498d953cc1b14b40c4a5db9260
Jiayu Zhao <jyzhao1230@g.ucla.edu>
2019-11-24 01:34:45 -0800

    [COMPONENT]: Insert grid into Profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,146 +1,168 @@
 <template>
-    <div>
-        <v-app id="inspire">
-            <v-card v-bind:style="cardObject">
-                <v-navigation-drawer
-                    v-model="sideNav"
-                    :mini-variant.sync="mini"
-                    permanent
-                    disable-route-watcher
-                    enable-resize-watcher
-                    :clipped="clipped"
-                    dark
-                    app
-                >
-                    <v-list-item>
-                        <v-list-item-avatar>
-                            <v-img
-                                src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
-                            ></v-img>
-                        </v-list-item-avatar>
-
-                        <v-list-item-title>Vibe User</v-list-item-title>
-
-                        <v-btn class="openclose" icon @click.stop="flipMini">
-                            <v-icon>mdi-chevron-left</v-icon>
-                        </v-btn>
-                    </v-list-item>
-
-                    <v-divider></v-divider>
-
-                    <v-list dense>
-                        <v-list-item v-for="item in items" :key="item.title" link>
-                            <v-list-item-icon>
-                                <v-icon>{{ item.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                            <v-list-item-content>
-                                <v-list-item-title>{{ item.title }}</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-navigation-drawer>
-                <div v-bind:style="userInfoObject">
-                    <figure class="image is-128x128" v-bind:style="profileImageObject">
-                        <img
-                            class="is-rounded"
-                            src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
-                        />
-                    </figure>
-                    <p v-bind:style="userNameObject">User Name</p>
-                </div>
-                <div v-bind:style="trackListObject">
-                    <li>track1</li>
-                    <li>track2</li>
-                </div>
-                <div v-bind:style="graphsObject">
-                    <p>Graphs place holder</p>
-                </div>
-            </v-card>
-        </v-app>
+  <div class="container">
+    <div class="item-user-profile">
+      <figure class="image is-128x128" v-bind:style="profileImageObject">
+        <img
+          class="is-rounded"
+          src="https://icon-library.net//images/default-user-icon/default-user-icon-9.jpg"
+        />
+      </figure>
+      <p v-bind:style="userNameObject">{{userName}}</p>
     </div>
+    <div class="item-song-list">
+      <p class="sub-item">Top Tracks</p>
+    </div>
+    <div class="item-header">
+      <p class="header">Analytics</p>
+    </div>
+    <div class="item-data">
+      <div class="data-item">
+        <p>100</p>
+        <p>Friends</p>
+      </div>
+      <div class="data-item">
+        <p>10</p>
+        <p>Songs</p>
+      </div>
+      <div class="data-item">
+        <p>12</p>
+        <p>Active Days</p>
+      </div>
+    </div>
+    <div class="item-main-graph"></div>
+    <div class="item-graph"></div>
+  </div>
 </template>
 
+<style type="text/css">
+.item-user-profile {
+  grid-area: user-profile;
+  position: relative;
+  border: 2px solid #66ff66;
+  border-radius: 12px;
+}
+
+.item-song-list {
+  grid-area: song-list;
+  border: 2px solid green;
+  border-radius: 12px;
+}
+
+.item-header {
+  grid-area: header;
+  border: 2px solid #ccff99;
+  border-radius: 12px;
+  position: relative;
+}
+
+.item-data {
+  grid-area: data;
+  border: 2px solid #80d4ff;
+  border-radius: 12px;
+}
+
+.item-main-graph {
+  grid-area: main-graph;
+  border: 2px solid blue;
+  border-radius: 12px;
+}
+
+.item-graph {
+  grid-area: graph;
+  border: 2px solid blueviolet;
+  border-radius: 12px;
+}
+
+.is-rounded {
+  max-width: 100%;
+  height: 50px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+
+.header {
+  max-width: 100%;
+  font-size: 24px;
+  line-height: 15px;
+  height: 20px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+
+.user-name {
+  float: left;
+  margin-left: 30px;
+}
+
+.data-item {
+  text-align: center;
+  padding-left: 130px;
+  padding-top: 25px;
+  padding-right: 100px;
+  float: left;
+}
+
+.app--view {
+  grid-row: view-start / view-end;
+  overflow-x: visible;
+}
+
+.container {
+  padding-top: 30px;
+  display: grid;
+  grid-column-gap: 10px;
+  grid-row-gap: 15px;
+  grid-template-columns: 300px 300px 200px 400px;
+  grid-template-rows: 100px 100px 200px 220px;
+  grid-template-areas:
+    "user-profile header header header"
+    "song-list data data data"
+    "song-list main-graph main-graph graph"
+    "song-list main-graph main-graph graph";
+}
+</style>
+
 <script>
-import {
-    VApp,
-    VCard,
-    VNavigationDrawer,
-    VListItem,
-    VListItemAvatar,
-    VImg,
-    VListItemTitle,
-    VListItemContent,
-    VBtn,
-    VIcon,
-    VDivider,
-    VList,
-    VListItemIcon
-    /* other imports ... */
-} from 'vuetify/lib';
+import { mapGetters } from "vuex";
 export default {
-    name: '',
-    components: {
-        VApp,
-        VCard,
-        VNavigationDrawer,
-        VListItem,
-        VListItemAvatar,
-        VImg,
-        VListItemTitle,
-        VBtn,
-        VIcon,
-        VDivider,
-        VList,
-        VListItemIcon,
-        VListItemContent
-    },
-    props: {
-        callMe: {
-            type: Function
-        }
-    },
-    data() {
-        return {
-            drawer: true,
-            items: [
-                { title: 'Feed', icon: 'mdi-home-city' },
-                { title: 'Profile', icon: 'mdi-account' },
-                { title: 'Search', icon: 'mdi-account-group-outline' }
-            ],
-            mini: true,
-            cardObject: {
-                height: '100%'
-            },
-            profileImageObject: {
-                width: '50px',
-                float: 'left'
-            },
-            userInfoObject: {
-                width: '100%',
-                marginTop: '75px',
-                marginLeft: '120px',
-                height: '100px'
-            },
-            userNameObject: {
-                display: 'table-cell',
-                paddingTop: '15px',
-                paddingLeft: '10px'
-            },
-            trackListObject: {
-                float: 'left'
-            },
-            graphsObject: {
-                float: 'left',
-                paddingLeft: '50px'
-            }
-        };
-    },
-    methods: {
-        flipMini() {
-            this.mini = !this.mini;
-            this.callMe('flip');
-        }
+  name: "",
+  components: {},
+  props: {
+    callMe: {
+      type: Function
     }
+  },
+  computed: {
+    ...mapGetters(["getUserId", "getUserName"])
+  },
+  data() {
+    return {
+      profileImageObject: {
+        paddingLeft: "40px",
+        width: "50px",
+        float: "left",
+        height: "100px"
+      },
+      userNameObject: {
+        display: "table-cell",
+        paddingTop: "35px",
+        paddingLeft: "40px",
+        float: "left",
+        marginLeft: "30px",
+        fontSize: "large"
+      },
+      userName: this.getUserName
+    };
+  },
+  methods: {
+    flipMini() {
+      this.mini = !this.mini;
+      this.callMe("flip");
+    }
+  }
 };
 </script>
