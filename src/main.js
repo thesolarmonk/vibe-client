eb37260f56b33838497e4a5a8ed9efb5d87b7c68
Tanuj <tanuj.lalwani@protonmail.com>
2019-11-27 21:52:34 -0800

    [FIX]: Squash small bugs

--- src/main.js
+++ src/main.js
@@ -7,17 +7,10 @@ import Buefy from 'buefy';
 Vue.use(Buefy);
 
 // import './../node_modules/bulma/css/bulma.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-
 Vue.config.productionTip = false;
-
-// Remove
-import vuetify from '@/plugins/vuetify';
-Vue.use(vuetify);
 
 new Vue({
   router,
   store,
-  vuetify,
   render: h => h(App)
 }).$mount('#app');
